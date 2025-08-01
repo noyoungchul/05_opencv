9744dc86209d44e05079b4745d50e908b835781c
noyoungchul <shdudcjf107@gmail.com>
2025-08-01 10:03:43 +0900

    Initial commit

---
+++ src/qr_scan.py
@@ -0,0 +1,14 @@
+import cv2
+import matplotlib.pylab as plt
+import pyzbar.pyzbar as pyzbar
+
+
+
+
+img = cv2.imread('../img/frame.png')
+
+plt.imshow(img)
+plt.show()
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
