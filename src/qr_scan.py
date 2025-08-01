3b0c90683d27f21f11f44463436420693abd2582
noyoungchul <shdudcjf107@gmail.com>
2025-08-01 11:13:44 +0900

    qr에 글자정보 넣기
    qr에 글자정보 넣기

--- src/qr_scan.py
+++ src/qr_scan.py
@@ -15,9 +15,14 @@ print(decoded)
 
 for d in decoded:
     print(d.data.decode('utf-8'))
+    barcode_data = d.data.decode('utf-8')
     print(d.type)
+    barcode_type = d.type
+
+    text = '%s (%s)' % (barcode_data, barcode_type)
 
     cv2.rectangle(img, (d.rect[0], d.rect[1]), (d.rect[0] + d.rect[2], d.rect[1] + d.rect[3]), (0,255,0), 20)
+    cv2.putText(img, text, (d.rect[0], d.rect[1] -50), cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 255, 0), 2, cv2.LINE_AA)
 
 plt.imshow(img)
 plt.show()
