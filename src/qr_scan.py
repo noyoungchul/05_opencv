1d53862d8dcd7a145f01900ed13e658ad10628d9
noyoungchul <shdudcjf107@gmail.com>
2025-08-01 10:43:20 +0900

    디코딩
    디코딩

--- src/qr_scan.py
+++ src/qr_scan.py
@@ -3,9 +3,15 @@ import matplotlib.pylab as plt
 import pyzbar.pyzbar as pyzbar
 
 img = cv2.imread('../img/frame.png')
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-plt.imshow(img)
+# plt.imshow(img)
+plt.imshow(gray, cmap='gray')
 plt.show()
+
+# 디코딩
+decoded = pyzbar.decode(gray)
+print(decoded)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
